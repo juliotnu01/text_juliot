da6e29027be96222c391e01ce7fb7174fb98b886
Juliot Nuñez <nunezjuliot@gmail.com>
2024-09-10 10:52:57 -0500

    primer commit

---
+++ resources/js/Components/TableDashboard.vue
@@ -0,0 +1,114 @@
+<script setup>
+
+</script>
+
+<template>
+    <table class="min-w-full table-auto bg-white shadow-md rounded-lg overflow-hidden">
+        <thead class="bg-gray-200">
+            <tr>
+                <th class="py-3 px-6 text-center">Employee ID</th>
+                <th class="py-3 px-6 text-center">Firstname</th>
+                <th class="py-3 px-6 text-center">Lastname</th>
+                <th class="py-3 px-6 text-center">Department</th>
+                <th class="py-3 px-6 text-center">Total access</th>
+                <th class="py-3 px-6 text-center">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="border-b">
+                <td class="py-4 px-6 text-center ">1000000</td>
+                <td class="py-4 px-6 text-center ">John</td>
+                <td class="py-4 px-6 text-center ">Doe</td>
+                <td class="py-4 px-6 text-center ">Department 1</td>
+                <td class="py-4 px-6 text-center ">99</td>
+                <td class="py-4 px-6 flex justify-center gap-2">
+                    <button class="flex items-center px-4 py-2 text-white bg-gray-500 hover:bg-gray-600 rounded gap-2">
+                        <svg fill="currentColor" width="20px" height="20px" viewBox="0 0 24 24" id="update-alt"
+                            data-name="Flat Line" xmlns="http://www.w3.org/2000/svg" class="icon flat-line">
+                            <path id="primary" d="M5.07,8A8,8,0,0,1,20,12"
+                                style="fill: none; stroke: white; stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;">
+                            </path>
+                            <path id="primary-2" data-name="primary" d="M18.93,16A8,8,0,0,1,4,12"
+                                style="fill: none; stroke: white; stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;">
+                            </path>
+                            <polyline id="primary-3" data-name="primary" points="5 3 5 8 10 8"
+                                style="fill: none; stroke: white; stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;">
+                            </polyline>
+                            <polyline id="primary-4" data-name="primary" points="19 21 19 16 14 16"
+                                style="fill: none; stroke: white; stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;">
+                            </polyline>
+                        </svg>
+                        Update
+                    </button>
+                    <button class="flex items-center px-4 py-2 text-white bg-gray-500 hover:bg-gray-600 rounded gap-2">
+                        <svg fill="currentColor" height="20px" width="20px" version="1.1" id="Layer_1"
+                            xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                            viewBox="0 0 472.615 472.615" xml:space="preserve">
+                            <g>
+                                <g>
+                                    <circle cx="147.692" cy="98.47" r="68.923" />
+                                </g>
+                            </g>
+                            <g>
+                                <g>
+                                    <path d="M271.266,200.647c-10.266-9.228-22.454-16.234-36.014-20.319l-15.854-4.629l-67.946,67.947l-68.045-67.947l-15.854,4.629
+			C27.178,192.539,0,228.974,0,271.12v83.407h211.226c-11.629-47.005,0.726-98.784,37.39-135.448
+			C255.6,212.093,263.203,205.959,271.266,200.647z" />
+                                </g>
+                            </g>
+                            <g>
+                                <g>
+                                    <path d="M436.618,233.005c-23.253-23.253-54.16-36.053-87.039-36.053c-32.878,0-63.786,12.8-87.039,36.053
+			c-47.995,47.996-47.995,126.082,0,174.078c23.994,23.993,55.517,35.985,87.039,35.985c31.523,0,63.046-11.992,87.039-35.985
+			C484.614,359.087,484.614,281.001,436.618,233.005z M246.182,320.044c0-27.619,10.751-53.584,30.283-73.115
+			c19.531-19.531,45.495-30.283,73.114-30.283c24.309,0,47.327,8.345,65.814,23.659L269.842,385.856
+			C254.527,367.369,246.182,344.352,246.182,320.044z M422.694,393.157c-37.953,37.954-98.327,40.137-138.912,6.608L429.317,254.23
+			c15.315,18.487,23.659,41.505,23.659,65.814C452.976,347.662,442.224,373.627,422.694,393.157z" />
+                                </g>
+                            </g>
+                        </svg>
+                        Disable
+                    </button>
+                    <button
+                        class="flex items-center px-4 py-2 text-white bg-yellow-500 hover:bg-yellow-600 rounded gap-2">
+                        <svg fill="currentColor" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+                            xmlns:xlink="http://www.w3.org/1999/xlink" width="20px" height="20px"
+                            viewBox="0 0 503.379 503.379" xml:space="preserve">
+                            <g>
+                                <path d="M458.091,128.116v326.842c0,26.698-21.723,48.421-48.422,48.421h-220.92c-26.699,0-48.421-21.723-48.421-48.421V242.439
+		c6.907,1.149,13.953,1.894,21.184,1.894c5.128,0,10.161-0.381,15.132-0.969v211.594c0,6.673,5.429,12.104,12.105,12.104h220.92
+		c6.674,0,12.105-5.432,12.105-12.104V128.116c0-6.676-5.432-12.105-12.105-12.105H289.835c0-12.625-1.897-24.793-5.297-36.315
+		h125.131C436.368,79.695,458.091,101.417,458.091,128.116z M159.49,228.401c-62.973,0-114.202-51.229-114.202-114.199
+		C45.289,51.229,96.517,0,159.49,0c62.971,0,114.202,51.229,114.202,114.202C273.692,177.172,222.461,228.401,159.49,228.401z
+		 M159.49,204.19c49.618,0,89.989-40.364,89.989-89.988c0-49.627-40.365-89.991-89.989-89.991
+		c-49.626,0-89.991,40.364-89.991,89.991C69.499,163.826,109.87,204.19,159.49,204.19z M227.981,126.308
+		c6.682,0,12.105-5.423,12.105-12.105s-5.423-12.105-12.105-12.105h-56.386v-47.52c0-6.682-5.423-12.105-12.105-12.105
+		s-12.105,5.423-12.105,12.105v59.625c0,6.682,5.423,12.105,12.105,12.105H227.981z M367.697,224.456h-131.14
+		c-6.682,0-12.105,5.423-12.105,12.105c0,6.683,5.423,12.105,12.105,12.105h131.14c6.685,0,12.105-5.423,12.105-12.105
+		C379.803,229.879,374.382,224.456,367.697,224.456z M367.91,297.885h-131.14c-6.682,0-12.105,5.42-12.105,12.105
+		s5.423,12.105,12.105,12.105h131.14c6.685,0,12.104-5.42,12.104-12.105S374.601,297.885,367.91,297.885z M367.91,374.353h-131.14
+		c-6.682,0-12.105,5.426-12.105,12.105c0,6.685,5.423,12.104,12.105,12.104h131.14c6.685,0,12.104-5.42,12.104-12.104
+		C380.015,379.778,374.601,374.353,367.91,374.353z" />
+                            </g>
+                        </svg>
+                        History
+                    </button>
+                    <button class="flex items-center px-4 py-2 text-white bg-red-500 hover:bg-red-600 rounded gap-2">
+                        <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path d="M9.1709 4C9.58273 2.83481 10.694 2 12.0002 2C13.3064 2 14.4177 2.83481 14.8295 4"
+                                stroke="currentColor" stroke-width="1.5" stroke-linecap="round" />
+                            <path d="M20.5001 6H3.5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" />
+                            <path
+                                d="M18.8332 8.5L18.3732 15.3991C18.1962 18.054 18.1077 19.3815 17.2427 20.1907C16.3777 21 15.0473 21 12.3865 21H11.6132C8.95235 21 7.62195 21 6.75694 20.1907C5.89194 19.3815 5.80344 18.054 5.62644 15.3991L5.1665 8.5"
+                                stroke="currentColor" stroke-width="1.5" stroke-linecap="round" />
+                            <path d="M9.5 11L10 16" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" />
+                            <path d="M14.5 11L14 16" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" />
+                        </svg>
+                        Delete
+                    </button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,107 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import Welcome from '@/Components/Welcome.vue';
+import TableDashboardVue from '@/Components/TableDashboard.vue';
+import { ref, onMounted, onUnmounted } from 'vue';
+
+const currentDate = ref(new Date().toLocaleString());
+const updateDateTime = () => {
+    currentDate.value = new Date().toLocaleString();
+};
+onMounted(() => {
+    const interval = setInterval(updateDateTime, 1000);
+    onUnmounted(() => {
+        clearInterval(interval);
+    });
+});
+
+</script>
+
+<template>
+    <AppLayout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Dashboard
+            </h2>
+        </template>
+        <div class="text-center w-full bg-[#cecece] capitalize font-bold ">
+            <p>administrative menu</p>
+        </div>
+        <div class="flex justify-between px-5">
+            <p class=" text-blue-500 font-bold  text-[16px] ">
+                {{ currentDate }}
+            </p>
+            <div class="flex gap-2 text-blue-500 font-bold  text-[16px] capitalize">
+                <p>
+                    Welcome :
+                </p>
+                <p class="text-blue-500 font-bold  text-[16px] capitalize"> {{ $page.props.auth.user.name }} </p>
+            </div>
+        </div>
+        <div class="flex mt-10 gap-4 px-6 justify-center">
+            <div class="flex flex-col w-2/12">
+                <label for="input_search_employee" class="mb-1 invisible">Placeholder</label>
+                <input type="text" class="h-10 px-2 border rounded" placeholder="Search by employee ID"
+                    id="input_search_employee">
+            </div>
+            <div class="flex flex-col w-2/12">
+                <label for="input_search_name" class="mb-1 invisible">Placeholder</label>
+                <input type="text" class="h-10 px-2 border rounded" placeholder="Search by Department"
+                    id="input_search_name">
+            </div>
+            <div class="flex flex-col w-2/12">
+                <label for="initial_access_date" class="mb-2 text-sm text-gray-700">Initial access date:</label>
+                <input type="date" class="h-10 px-2 border rounded" placeholder="Start date" id="initial_access_date">
+            </div>
+            <div class="flex flex-col w-2/12">
+                <label for="final_access_date" class="mb-2 text-sm text-gray-700">Final access date:</label>
+                <input type="date" class="h-10 px-2 border rounded" placeholder="End date" id="final_access_date">
+            </div>
+            <div class="flex items-end gap-2">
+                <button class="px-4 py-[5px] text-white bg-gray-500 rounded hover:bg-gray-600 flex gap-2 ">
+                    <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
+                        class=" self-center ">
+                        <path
+                            d="M14 20.5H10C9.80189 20.4974 9.61263 20.4176 9.47253 20.2775C9.33244 20.1374 9.25259 19.9481 9.25 19.75V12L3.9 4.69C3.81544 4.58007 3.76395 4.44832 3.75155 4.31018C3.73915 4.17204 3.76636 4.03323 3.83 3.91C3.89375 3.78712 3.98984 3.68399 4.10792 3.61173C4.226 3.53947 4.36157 3.50084 4.5 3.5H19.5C19.6384 3.50084 19.774 3.53947 19.8921 3.61173C20.0101 3.68399 20.1062 3.78712 20.17 3.91C20.2336 4.03323 20.2608 4.17204 20.2484 4.31018C20.236 4.44832 20.1846 4.58007 20.1 4.69L14.75 12V19.75C14.7474 19.9481 14.6676 20.1374 14.5275 20.2775C14.3874 20.4176 14.1981 20.4974 14 20.5ZM10.75 19H13.25V11.75C13.2492 11.5907 13.302 11.4357 13.4 11.31L18 5H6L10.62 11.31C10.718 11.4357 10.7708 11.5907 10.77 11.75L10.75 19Z"
+                            fill="currentColor" />
+                    </svg>
+                    <p class=" self-center ">
+                        Filter
+                    </p>
+                </button>
+                <button class="px-4 py-[5px] text-white bg-gray-500 rounded hover:bg-gray-600 flex gap-2 ">
+                    <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
+                        class=" self-center ">
+                        <path
+                            d="M10.0303 8.96967C9.73741 8.67678 9.26253 8.67678 8.96964 8.96967C8.67675 9.26256 8.67675 9.73744 8.96964 10.0303L10.9393 12L8.96966 13.9697C8.67677 14.2626 8.67677 14.7374 8.96966 15.0303C9.26255 15.3232 9.73743 15.3232 10.0303 15.0303L12 13.0607L13.9696 15.0303C14.2625 15.3232 14.7374 15.3232 15.0303 15.0303C15.3232 14.7374 15.3232 14.2625 15.0303 13.9697L13.0606 12L15.0303 10.0303C15.3232 9.73746 15.3232 9.26258 15.0303 8.96969C14.7374 8.6768 14.2625 8.6768 13.9696 8.96969L12 10.9394L10.0303 8.96967Z"
+                            fill="currentColor" />
+                        <path fill-rule="evenodd" clip-rule="evenodd"
+                            d="M12.0574 1.25H11.9426C9.63424 1.24999 7.82519 1.24998 6.41371 1.43975C4.96897 1.63399 3.82895 2.03933 2.93414 2.93414C2.03933 3.82895 1.63399 4.96897 1.43975 6.41371C1.24998 7.82519 1.24999 9.63422 1.25 11.9426V12.0574C1.24999 14.3658 1.24998 16.1748 1.43975 17.5863C1.63399 19.031 2.03933 20.1711 2.93414 21.0659C3.82895 21.9607 4.96897 22.366 6.41371 22.5603C7.82519 22.75 9.63423 22.75 11.9426 22.75H12.0574C14.3658 22.75 16.1748 22.75 17.5863 22.5603C19.031 22.366 20.1711 21.9607 21.0659 21.0659C21.9607 20.1711 22.366 19.031 22.5603 17.5863C22.75 16.1748 22.75 14.3658 22.75 12.0574V11.9426C22.75 9.63423 22.75 7.82519 22.5603 6.41371C22.366 4.96897 21.9607 3.82895 21.0659 2.93414C20.1711 2.03933 19.031 1.63399 17.5863 1.43975C16.1748 1.24998 14.3658 1.24999 12.0574 1.25ZM3.9948 3.9948C4.56445 3.42514 5.33517 3.09825 6.61358 2.92637C7.91356 2.75159 9.62177 2.75 12 2.75C14.3782 2.75 16.0864 2.75159 17.3864 2.92637C18.6648 3.09825 19.4355 3.42514 20.0052 3.9948C20.5749 4.56445 20.9018 5.33517 21.0736 6.61358C21.2484 7.91356 21.25 9.62177 21.25 12C21.25 14.3782 21.2484 16.0864 21.0736 17.3864C20.9018 18.6648 20.5749 19.4355 20.0052 20.0052C19.4355 20.5749 18.6648 20.9018 17.3864 21.0736C16.0864 21.2484 14.3782 21.25 12 21.25C9.62177 21.25 7.91356 21.2484 6.61358 21.0736C5.33517 20.9018 4.56445 20.5749 3.9948 20.0052C3.42514 19.4355 3.09825 18.6648 2.92637 17.3864C2.75159 16.0864 2.75 14.3782 2.75 12C2.75 9.62177 2.75159 7.91356 2.92637 6.61358C3.09825 5.33517 3.42514 4.56445 3.9948 3.9948Z"
+                            fill="currentColor" />
+                    </svg>
+                    <p class=" self-center ">
+                        Clear Filter
+                    </p>
+                </button>
+            </div>
+        </div>
+
+        <div class="border-solid border-[1px] border-black w-11/12 mx-auto my-10" />
+
+        <div class="w-full  px-6 flex justify-end ">
+            <button class="px-4 py-[10px] text-white bg-blue-500 rounded hover:bg-blue-600 flex gap-2 ">
+                <p class=" self-center ">
+                    New employee
+                </p>
+            </button>
+        </div>
+
+        <div class="py-4">
+            <div class=" mx-auto px-6 ">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    <TableDashboardVue />
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
